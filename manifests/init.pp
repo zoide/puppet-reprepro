3709ce1c698515b247e783c7daa95819b34a6b21
Steffen Zieger <me@saz.sh>
2012-03-09 12:25:05 +0100

    add lenny as supported platform

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,7 @@ class reprepro {
   case $operatingsystem {
     Debian: {
       case $lsbdistcodename {
-        squeeze: { include reprepro::debian }
+        squeeze, lenny: { include reprepro::debian }
         default: { fail "reprepro is not available for ${operatingsystem}/${lsbdistcodename}"}
       }
     }
