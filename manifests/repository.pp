1640ba4ee7353c038bac5bfb3b5e42c4219d72fe
Joe Topjian <joe@topjian.net>
2012-11-30 19:01:22 +0000

    Cleaned up repository.pp, added options option.

--- manifests/repository.pp
+++ manifests/repository.pp
@@ -28,69 +28,71 @@ Example usage:
       description   => "Camptocamp consolidated lenny-backports dev-repository",
       sign_with     => "packages@mycompagny",
       update        => "lenny-backports",
+      options       => ['verbose', 'basedir .'],
     }
 
 */
 define reprepro::repository (
-  $ensure=present,
-  $incoming_name="incoming",
-  $incoming_dir="incoming",
-  $incoming_tmpdir="tmp",
-  $incoming_allow=""
+  $ensure          = present,
+  $incoming_name   = "incoming",
+  $incoming_dir    = "incoming",
+  $incoming_tmpdir = "tmp",
+  $incoming_allow  = "",
+  $options         = ['verbose', 'ask-passphrase', 'basedir .']
   ) {
 
   include reprepro::params
 
   file {
     [
-      "${reprepro::params::basedir}/${name}/conf",
-      "${reprepro::params::basedir}/${name}/lists",
-      "${reprepro::params::basedir}/${name}/db",
-      "${reprepro::params::basedir}/${name}/logs",
-      "${reprepro::params::basedir}/${name}/tmp",
+      "${::reprepro::params::basedir}/${name}/conf",
+      "${::reprepro::params::basedir}/${name}/lists",
+      "${::reprepro::params::basedir}/${name}/db",
+      "${::reprepro::params::basedir}/${name}/logs",
+      "${::reprepro::params::basedir}/${name}/tmp",
     ]:
       ensure  => $ensure ? { present => directory, default => $ensure,},
       purge   => $ensure ? { present => undef,     default => true,}, 
       recurse => $ensure ? { present => undef,     default => true,},
       force   => $ensure ? { present => undef,     default => true,},
-      mode    => 2750,
-      owner   => reprepro, 
-      group   => reprepro;
+      mode    => '2750',
+      owner   => 'reprepro', 
+      group   => 'reprepro';
        
     [
-      "${reprepro::params::basedir}/${name}",
-      "${reprepro::params::basedir}/${name}/dists",
-      "${reprepro::params::basedir}/${name}/pool",
+      "${::reprepro::params::basedir}/${name}",
+      "${::reprepro::params::basedir}/${name}/dists",
+      "${::reprepro::params::basedir}/${name}/pool",
     ]:
       ensure  => $ensure ? { present => directory, default => $ensure,},
       purge   => $ensure ? { present => undef,     default => true,}, 
       recurse => $ensure ? { present => undef,     default => true,},
       force   => $ensure ? { present => undef,     default => true,},
-      mode    => 2755, 
-      owner   => reprepro, 
-      group   => reprepro;
+      mode    => '2755', 
+      owner   => 'reprepro', 
+      group   => 'reprepro';
 
-    "${reprepro::params::basedir}/${name}/incoming":
+    "${::reprepro::params::basedir}/${name}/incoming":
       ensure  => $ensure ? { present => directory, default => $ensure,},
       purge   => $ensure ? { present => undef,     default => true,}, 
       recurse => $ensure ? { present => undef,     default => true,},
       force   => $ensure ? { present => undef,     default => true,},
-      mode    => 2770,
-      owner   => reprepro,
-      group   => reprepro;
+      mode    => '2770',
+      owner   => 'reprepro',
+      group   => 'reprepro';
 
-    "${reprepro::params::basedir}/${name}/conf/options":
+    "${::reprepro::params::basedir}/${name}/conf/options":
       ensure  => $ensure,
-      mode    => 0640,
-      owner   => reprepro,
-      group   => reprepro,
-      content => "verbose\nask-passphrase\nbasedir .\n";
+      mode    => '0640',
+      owner   => 'reprepro',
+      group   => 'reprepro',
+      content => inline_template("<%= $options.join(\"\n\") %>");
 
-    "${reprepro::params::basedir}/${name}/conf/incoming":
+    "${::reprepro::params::basedir}/${name}/conf/incoming":
       ensure  => $ensure,
-      mode    => 0640,
-      owner   => reprepro,
-      group   => reprepro,
+      mode    => '0640',
+      owner   => 'reprepro',
+      group   => 'reprepro',
       content => template("reprepro/incoming.erb");
   }
 
